337b07691cfdaeeed192b84686ebaba4ce2c5364
TheJerma45 <Jermaine1999@live.nl>
2016-10-24 11:54:25 +0200

    Ordening Update
    Alles Geordered

---
+++ P&P/Assets/PullScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	void Update () {
+	//als speler wil ik als ik op A klik dat ik het blok naar mij toe trek
+	}
+}
---
+++ P&P/Assets/PushScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushScript : MonoBehaviour {
+
+	void Update () {
+	//als speler wil ik zorgen dat als ik op L Klik ik het blok van mij weg duwt
+	}
+}
